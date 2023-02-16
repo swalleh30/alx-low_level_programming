6401be2139839f8566cfc21302c6a9712d2f1c26
swalleh30 <phaeezshee@gmail.com>
2023-02-16 13:00:42 -0800

    task.5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - this program prints a line with the help of printf
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
