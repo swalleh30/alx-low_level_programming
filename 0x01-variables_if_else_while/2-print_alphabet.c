7b2603578ec2e983b738b2897d25b1a3ad7422bc
swalleh30 <phaeezshee@gmail.com>
2023-02-17 12:26:39 -0800

    task.2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -the same task again
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+	printf("%s\n", alphabet);
+	return (0);
+}
+
